281b1a27671772da4affe59e1f25b148f3ff3130
Chloe Umble <chloe@thehappyhome.co>
2016-05-04 17:57:32 -0700

    minor changes

--- bot.py
+++ bot.py
@@ -1,67 +1,49 @@
+import os
 import tweepy
 from secrets import *
-from random import choice
-import os
-import re
-
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-tweeted_file = os.path.join(__location__, "tweeted_users.txt")
-
-data = {'like':
-            {'queries': ['"poop"'],
-             'responses': [u'\E4F5']
-            }
-        }
-
-filters = ['']
+from time import gmtime, strftime
 
 
-def get_tweet(api_, type_):
-    """Get a list of tweets matching the search query."""
-    query = choice(data[type_]['queries'])
-    results = api_.search(q=query, count=50)
-    return results
+# ====== Individual bot configuration ==========================
+bot_username = 'GoCallGethard'
+logfile_name = bot_username + ".log"
+auth = tweepy.OAuthHandler(C_KEY, C_SECRET)  
+auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)  
+api = tweepy.API(auth)  
+# ==============================================================
 
 
-def get_users():
-    """Get a list of users we've already tweeted at."""
-    f = open(tweeted_file, 'r')
-    usrs = [line.rstrip('\n') for line in f]
-    return usrs
+def create_tweet():
+    """Create the text of the tweet you want to send."""
+    # Replace this with your code!
+    text = ""
+    return text
 
 
-def filter_tweets(tweets_, users_):
-    """Filter out tweets... etc."""
-    while True:
-        tweet_ = tweets_.pop(0)
-        text = tweet_.text
-        if len(tweets) == 0:
-            return
-        if not (hasattr(tweet_, "retweeted_status") or
-                tweet_.in_reply_to_status_id or
-                tweet_.author.screen_name in users_ or
-                any(substr in text.lower() for substr in filters)):
-            return tweet_
-
-
-def send_reply(api_, type_, tweet_):
-    """Send the reply tweet and record it."""
-    f = open(tweeted_file, 'a')
-    f.write(tweet_.author.screen_name + '\n')
-    f.close()
-    text = '@' + tweet_.author.screen_name + ' ' + choice(data[type_]['responses'])
-    api_.update_status(text, in_reply_to_status_id=tweet_.id_str)
-
-
-if __name__ == "__main__":
-    """Find a tweet and reply to it."""
+def tweet(text):
+    """Send out the text as a tweet."""
+    # Twitter authentication
     auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
     auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
     api = tweepy.API(auth)
 
-    tweet_type = choice(data.keys())
+    # Send the tweet and log success or failure
+    try:
+        api.update_status(text)
+    except tweepy.error.TweepError as e:
+        log(e.message)
+    else:
+        log("Tweeted: " + text)
 
-    tweets = get_tweet(api, tweet_type)
-    users = get_users()
-    tweet = filter_tweets(tweets, users)
-    send_reply(api, tweet_type, tweet)
+
+def log(message):
+    """Log message to logfile."""
+    path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    with open(os.path.join(path, logfile_name), 'a+') as f:
+        t = strftime("%d %b %Y %H:%M:%S", gmtime())
+        f.write("\n" + t + " " + message)
+
+
+if __name__ == "__main__":
+    tweet_text = create_tweet()
+    tweet(tweet_text)
